4eb8c926e0e1126407c064be3934088a11a96135
beMimg <bernardommguerreiro@gmail.com>
2024-05-09 16:43:41 +0200

    Create route and controller to edit a pizza

--- src/routes/pizza.ts
+++ src/routes/pizza.ts
@@ -1,15 +1,18 @@
-import express, { Request, Response, Router } from "express";
+import express, { Router } from "express";
+import { authJwt } from "../middleware/jwtStrategy";
 import {
   createPizza,
   pizzaValidation,
+  editPizza,
   getAllPizzas,
 } from "../controllers/pizza";
-import { authJwt } from "../middleware/jwtStrategy";
 
 const router: Router = express.Router();
 
 router.get("/", getAllPizzas);
 
 router.post("/", authJwt, pizzaValidation, createPizza);
+
+router.put("/:id", authJwt, pizzaValidation, editPizza);
 
 export default router;
